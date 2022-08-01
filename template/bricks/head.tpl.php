b381b2fccba6b7b5e210796119d3a9028ec7d5fc
Pasi <pasi.honkala@actamedia.fi>
2022-08-01 16:03:42 +0300

    Update Google Analytics tag to use Universal Analytics (gtag.js)

--- template/bricks/head.tpl.php
+++ template/bricks/head.tpl.php
@@ -62,15 +62,16 @@ endif;
 <?php
 if (CFG_ANALYTICS_USER):
 ?>
-    <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+<!-- Global Site Tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-        ga('create', '<?=CFG_ANALYTICS_USER; ?>', 'auto');
-        ga('send', 'pageview');
-    </script>
+  gtag('config', '<?=CFG_ANALYTICS_USER; ?>');
+</script>
+<!-- End Global Site Tag (gtag.js) - Google Analytics -->
 <?php
 endif;
 ?>
